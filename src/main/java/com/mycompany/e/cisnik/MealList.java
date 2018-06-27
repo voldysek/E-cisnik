81358ebbeb6faad026d4f6d3a935eac2b1605f8a
Petr Voldan <voldysek1@seznam.cz>
2018-06-27 15:24:05 +0200

    gui a v jedne tride je metoda ukradene od davida

--- src/main/java/com/mycompany/e/cisnik/MealList.java
+++ src/main/java/com/mycompany/e/cisnik/MealList.java
@@ -5,10 +5,50 @@
  */
 package com.mycompany.e.cisnik;
 
+import java.util.ArrayList;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author voldy
  */
 public class MealList {
     
-}
+    
+    
+    /* 
+    public void vypisDoTabulky(JTable tabulka)
+    {
+        
+        DefaultTableModel modelTabulky = (DefaultTableModel)tabulka.getModel();
+        //Nejdříve nastaví počet řádků a slouců tabulky na 0.
+        modelTabulky.setRowCount(0);
+        modelTabulky.setColumnCount(0);
+        
+        //Definování pole typu Object o velikosti aktuálního počtu sloupců
+        Object[] dataTabulky = new Object[pocetSloupcu];
+        
+        //Do tabulky přidá názvy sloupců
+        for(int i = 0; i< pocetSloupcu; i++)
+        {
+            modelTabulky.addColumn(nazvySloupcu.get(i));
+        }
+      
+        //Cyklus, který projede všechny ArrayListy(řádky) v ArrayListu dat
+        for(ArrayList radka: data)
+        {
+           for(int i = 0; i< pocetSloupcu; i++) 
+           {
+                dataTabulky[i] = radka.get(i); //Do pole přidá všechna data v řádce
+           }
+           modelTabulky.addRow(dataTabulky); //Do tabulky přidá novou řádku
+        }
+            
+        //nastaví tabulce model, do kterého se právě přidala všechna data
+        tabulka.setModel(modelTabulky);
+        }
+*/
+    }
+    
+
