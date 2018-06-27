5f51a18cb6ba7a61c30b27498b1ae24b94a8d22c
Petr Voldan <voldysek1@seznam.cz>
2018-06-27 15:11:04 +0200

    pridani trid

---
+++ src/main/java/com/mycompany/e/cisnik/E_Waiter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.e.cisnik;
+
+/**
+ *
+ * @author voldy
+ */
+public class E_Waiter {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world");
+        
+        
+    }
+    
+}
