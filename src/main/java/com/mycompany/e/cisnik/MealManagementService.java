64a95b7ef7cb5fbd1933b189e2021573563fa511
Petr Voldan <voldysek1@seznam.cz>
2018-06-28 12:05:47 +0200

    uprava

--- src/main/java/com/mycompany/e/cisnik/MealManagementService.java
+++ src/main/java/com/mycompany/e/cisnik/MealManagementService.java
@@ -11,20 +11,12 @@ package com.mycompany.e.cisnik;
  */
 public interface MealManagementService {
 
-<<<<<<< HEAD:src/main/java/com/mycompany/e/cisnik/MealManagementService.java
+    /**
+     * Metoda, pomocí které se přidá jídlo do seznamu.
+     *
+     * @param meal Jídlo, které se přidává do seznamu.
+     */
     public void addMeal(Meal meal);
 
     public void listMeals();
-=======
-    /**
-     * Metoda, pomocí které se přidá jídlo do seznamu.
-     * @param meal Jídlo, které se přidává do seznamu.
-     */
-    public void addToMealList(Meal meal);
-
-    /**
-     * Metoda, která vypíše celý list na obrazovku.
-     */
-    public void listMealList();
->>>>>>> abae0262420e316d05f1f3c8cd8ee8025ddbc470:src/main/java/com/mycompany/e/cisnik/MealListInterface.java
 }
